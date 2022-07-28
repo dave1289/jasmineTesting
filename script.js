043d1e9fc8122f8bac6ce760475eb245bc2d2619
dave <davemcelhaney@gmail.com>
2022-07-28 17:57:38 -0400

    testing matchers

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function calculateTaxes(income) {
+    if (!Number.isFinite(income)){
+        throw new Error('Invalid income!')
+    }
     if (income > 30000) {
         return income * 0.25
     }
@@ -8,3 +11,20 @@ function calculateTaxes(income) {
 }
 
 console.log(calculateTaxes(500))
+
+function removeDupes(values) {
+    const arr = [...new Set(values)]
+    if(typeof values === 'string'){
+        return arr.join('')
+    }
+    return arr;
+}
+
+const testArr = [1,2,3,4,5,4,4,4]
+[1,2,3,4,5]
+
+function remove(arr, val) {
+    return arr.filter((el) => {
+        el !== val;
+    })
+}
--- script.test.js
+++ script.test.js
@@ -3,6 +3,22 @@ it('should calculate low-bracket', function (){
     expect(calculateTaxes(20000)).toEqual(3000);
 })
 it('should calculate high-bracket', function (){
-    expect(calculateTaxes(50000)).toEqual(50000 * 0.25);
-    expect(calculateTaxes(80000)).toEqual(20000);
+    expect(calculateTaxes(50000)).toBe(12500);
+    expect(calculateTaxes(80000)).toBe(20000);
+})
+
+it('should remove dupes from arr', function() {
+    expect(removeDupes([1,2,3,4,4,4])).toEqual([1,2,3,4])
+    expect(removeDupes('slaaaaaam')).toBe('slam')
+    expect(removeDupes('hello')).toBeInstanceOf(String)
+})
+
+it('remove value from array', function() {
+    expect(remove([1,2,3,4,5,6], 6)).not.toContain(6);
+})
+
+it('should give invalid income error', function() {
+    expect(() => calculateTaxes('asdgasd')).toThrowError()
+    expect(() => calculateTaxes([])).toThrowError()
+    expect(() => calculateTaxes(true)).toThrowError()
 })
