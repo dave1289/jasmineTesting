faddcfde111247c0a87c3352f4303a6ce689d5ad
dave <davemcelhaney@gmail.com>
2022-07-28 17:29:53 -0400

    tried arithmetic vs static value for testing

--- script.test.js
+++ script.test.js
@@ -3,6 +3,6 @@ it('should calculate low-bracket', function (){
     expect(calculateTaxes(20000)).toEqual(3000);
 })
 it('should calculate high-bracket', function (){
-    expect(calculateTaxes(50000)).toEqual(12500);
+    expect(calculateTaxes(50000)).toEqual(50000 * 0.25);
     expect(calculateTaxes(80000)).toEqual(20000);
 })
